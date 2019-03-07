7e59d4bb98c71a08b4ad781f5fd071d7536c623b
aburk3 <aburk3@gmail.com>
2019-03-07 06:59:41 -0600

    Added columns to user table

--- db/migrate/20190225225230_create_users.rb
+++ db/migrate/20190225225230_create_users.rb
@@ -5,8 +5,16 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :last_name
       t.string :email
       t.string :password_digest
+      t.string :club
+      t.string :state
+      t.string :city
+      t.float :utpr
+      t.float :skill
+      t.integer :score
       t.string :image
       t.string :uid
+      t.boolean :admin
+      t.boolean :trainer
 
       t.timestamps
     end
