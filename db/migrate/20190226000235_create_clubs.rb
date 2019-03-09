8bf5d80e41c98b758f1049e650c15080b9c6043b
aburk3 <aburk3@gmail.com>
2019-03-09 00:14:47 -0600

    Added fields to database

--- db/migrate/20190226000235_create_clubs.rb
+++ db/migrate/20190226000235_create_clubs.rb
@@ -4,6 +4,9 @@ class CreateClubs < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :club_category
       t.string :description
+      t.string :state
+      t.string :city
+      t.string :contact
       t.integer :admin
       t.boolean :has_max, default: false
 
