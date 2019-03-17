9c6884aaf4a7c1794ece9aff27457b0ba173d8c2
aburk3 <aburk3@gmail.com>
2019-03-17 14:35:10 -0500

    Removed old search bar and added one with filters and more search_controller actions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,29 @@
 class SearchController < ApplicationController
 
-  def index
+  def search_users
     if params[:search].blank?
       redirect_to(root_path, alert: "Empty field!") and return
     else
       @parameter = params[:search].downcase
       @results = User.all.where("lower(first_name) LIKE :search", search: @parameter)
-      binding.pry
+    end
+  end
+
+  def search_clubs
+    if params[:search].blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    else
+      @parameter = params[:search].downcase
+      @results = User.all.where("lower(first_name) LIKE :search", search: @parameter)
+    end
+  end
+
+  def search_clubs
+    if params[:search].blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    else
+      @parameter = params[:search].downcase
+      @results = User.all.where("lower(first_name) LIKE :search", search: @parameter)
     end
   end
 
