9ab760f150d1b8b18fd17b9c3a2577b573a7fea5
aburk3 <aburk3@gmail.com>
2019-03-17 14:05:55 -0500

    Added search params to the form_tag

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
 
   def search
+    raise params.inspect
+    if params[:search].blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    else
 
+    end
   end
 
 end
