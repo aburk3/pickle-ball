835d8a103fda9da50e3ec9da482bfa8167ef3c0a
aburk3 <aburk3@gmail.com>
2019-03-17 16:07:42 -0500

    Added logic to search by users last name as well as first

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
     else
       parameter = params[:search].downcase
       @clubs = Club.all.where("lower(name) LIKE :search", search: "%#{parameter}%")
-      @users = User.all.where("lower(first_name) LIKE :search", search: "%#{parameter}%")
+      @users = User.all.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search", search: "%#{parameter}%")
       @tournaments = Tournament.all.where("lower(name) LIKE :search", search: "%#{parameter}%")
     end
   end
