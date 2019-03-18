1532dc35170cf955594dc74c25ad80c0f2de87fc
aburk3 <aburk3@gmail.com>
2019-03-18 05:34:38 -0500

    added table for categories and post_categories join table

---
+++ db/migrate/20190318103302_create_post_categories.rb
@@ -0,0 +1,9 @@
+class CreatePostCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :post_categories do |t|
+      t.belongs_to :post, index: true, foreign_key: true
+      t.belongs_to :category, index: true, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_101640) do
+ActiveRecord::Schema.define(version: 2019_03_18_103302) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clubs", force: :cascade do |t|
     t.string "name"
@@ -43,6 +49,15 @@ ActiveRecord::Schema.define(version: 2019_03_18_101640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["club_id"], name: "index_meetings_on_club_id"
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_post_categories_on_category_id"
+    t.index ["post_id"], name: "index_post_categories_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
