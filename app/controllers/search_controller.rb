db55b3b286989a44cf3b1646380731863cd3b76f
aburk3 <aburk3@gmail.com>
2019-03-19 05:28:07 -0500

    Added sql query for when first and last name are both searched together

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,19 @@ class SearchController < ApplicationController
       redirect_to(root_path, alert: "Empty field!") and return
     else
       parameter = params[:search].downcase
-      @clubs = Club.all.where("lower(name) LIKE :search", search: "%#{parameter}%")
+      @clubs = Club.all.where("lower(name) LIKE :search",
+       search: "%#{parameter}%"
+      )
       @users = User.all.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search", search: "%#{parameter}%")
+      if @users.count == 0
+        split_string = parameter.scan(/\w+/).values_at(0, -1)
+        first_name = split_string[0]
+        last_name = split_string[1]
+        @users = User.where(
+          "lower(first_name) LIKE :first_name AND lower(last_name) LIKE :last_name",
+          first_name: "%#{first_name}%", last_name: "%#{last_name}%"
+          )
+      end
       @tournaments = Tournament.all.where("lower(name) LIKE :search", search: "%#{parameter}%")
     end
   end
