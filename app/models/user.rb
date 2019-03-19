e86a590ac3de37fd3758692aed8a6252d3687067
aburk3 <aburk3@gmail.com>
2019-03-19 05:33:11 -0500

    added association to user model for user_tournaments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
   has_many :user_clubs
   has_many :clubs, :through => :user_clubs
+
+  has_many :user_tournaments
+  has_many :tournaments, :through => :user_tournaments
+
   validates :first_name, presence: true, on: :create
   validates :last_name, presence: true, on: :create
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, on: :create
