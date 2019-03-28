6b6bfe8ea8fa5f276b6537e276e6ff93c065be98
aburk3 <aburk3@gmail.com>
2019-03-28 18:22:52 -0500

    Edited post comment form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_001704) do
+ActiveRecord::Schema.define(version: 2019_03_28_175131) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_03_27_001704) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
