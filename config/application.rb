23767b17bf837bd347da7ff0e9d78fa0a8d3c9c0
aburk3 <aburk3@gmail.com>
2019-06-08 17:13:45 -0500

    Testing moving development and production actionmailer config to application.rb file

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,16 @@ module Pickleball
       end if File.exists?(env_file)
     end
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            ENV["GMAIL_USERNAME"],
+      password:             ENV["GMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
