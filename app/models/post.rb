4b84c39270386d4a8b71a6a8033ce981a6aef2f6
aburk3 <aburk3@gmail.com>
2019-06-08 17:58:17 -0500

    Changed the length validation for post descriptions

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  validates :title, presence: true, :length => {:within => 5..50}, on: :create
-  validates :content, presence: true, :length => {:within => 5..50}, on: :create
+  validates :title, presence: true, :length => {:within => 5..100}, on: :create
+  validates :content, presence: true, :length => {:within => 5..500}, on: :create
 end
