445499f6cccbde045f943bfad0201730a1db14a0
aburk3 <aburk3@gmail.com>
2019-06-13 11:55:04 -0500

    Added external link to USAPA venues site, changed length contraint for creating tournaments and clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -5,7 +5,7 @@ class Club < ActiveRecord::Base
 
   scope :open_clubs, -> { where(status: true) }
 
-  validates :name, presence: true, :length => {:within => 5..30}, on: :create
+  validates :name, presence: true, :length => {:within => 5..100}, on: :create
 
   def is_admin?(user)
     self.club_admin == user.id
