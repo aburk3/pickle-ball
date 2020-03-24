1a3956bcd8157075374720681be7dec0e808f007
aburk3 <aburk3@gmail.com>
2020-03-24 07:57:16 -0500

    Fixed nav bar drop downs requiring double click.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,7 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("jquery");
-require("./bootstrap");
+// require("./bootstrap");
 
 // require("custom");
 
